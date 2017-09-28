c5d63b554ee2abf48f7eda598570ea756dcfc857
Naoto Kaneko <naoty.k@gmail.com>
2017-09-28 20:28:44 +0900

    Embed sourcemaps into css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require("gulp");
 var concat = require("gulp-concat");
 var cleanCSS = require("gulp-clean-css");
+var sourcemaps = require("gulp-sourcemaps");
 
 gulp.task("default", function() {
   return gulp.src(["node_modules/normalize.css/normalize.css", "src/style.css"])
-    .pipe(concat("style.css"))
-    .pipe(cleanCSS())
+    .pipe(sourcemaps.init())
+      .pipe(concat("style.css"))
+      .pipe(cleanCSS())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest("./dist"));
 });
