710dbe729e8119537f888ba6369c6ae28e3285a2
Naoto Kaneko <naoty.k@gmail.com>
2017-09-28 19:48:55 +0900

    Concat normalize.css and style.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require("gulp");
+var concat = require("gulp-concat");
 
 gulp.task("default", function() {
-  console.log("Hello, gulp.js");
+  return gulp.src(["node_modules/normalize.css/normalize.css", "src/style.css"])
+    .pipe(concat("style.css"))
+    .pipe(gulp.dest("./dist"));
 });
