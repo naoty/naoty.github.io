7635b511cb76e6d3bd903fb6a872d7b7a817ea9c
Naoto Kaneko <naoty.k@gmail.com>
2017-09-28 19:57:07 +0900

    Minify stylesheets with gulp-clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require("gulp");
 var concat = require("gulp-concat");
+var cleanCSS = require("gulp-clean-css");
 
 gulp.task("default", function() {
   return gulp.src(["node_modules/normalize.css/normalize.css", "src/style.css"])
     .pipe(concat("style.css"))
+    .pipe(cleanCSS())
     .pipe(gulp.dest("./dist"));
 });
